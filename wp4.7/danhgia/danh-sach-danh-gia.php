9a9cc4b2a48be626a80bef63477cd4a5bf416543
Hunghbm <huynhbamanhhungg@gmail.com>
2017-09-12 09:53:49 +0700

    feat(custom): Custom string data Custom string data see #3

--- wp4.7/danhgia/danh-sach-danh-gia.php
+++ wp4.7/danhgia/danh-sach-danh-gia.php
@@ -6,13 +6,21 @@ $arr = select_elements('#tasks .li_canbo img', $html);
 $data = array();
 
 foreach ($arr as $key => $value) {
-    $to_encode = array('name' => select_elements('#tasks .li_canbo .lbl:nth-child(1)', $html)[$key]["text"],
-      'dOB' => select_elements('#tasks .li_canbo .lbl:nth-child(2)', $html)[$key]["text"],
-      'image' => $value["attributes"]["src"],
-      'level' => select_elements('#tasks .li_canbo .lbl:nth-child(3)', $html)[$key]["text"],
-      'position' => select_elements('#tasks .li_canbo .lbl:nth-child(4)', $html)[$key]["text"],
-    );
-    array_push($data,$to_encode);
+  $name = select_elements('#tasks .li_canbo .lbl:nth-child(1)', $html)[$key]["text"];
+  $dOB = str_replace("Ngày sinh ","",select_elements('#tasks .li_canbo .lbl:nth-child(2)', $html)[$key]["text"]);
+  $image = $value["attributes"]["src"];
+  $level = str_replace("Trình độ học vấn: ","",select_elements('#tasks .li_canbo .lbl:nth-child(3)', $html)[$key]["text"]);
+  $position = str_replace("Chức vụ: ","",select_elements('#tasks .li_canbo .lbl:nth-child(4)', $html)[$key]["text"]);
+
+  $to_encode = array(
+    'id' => $key,
+    'name' => $name,
+    'dOB' => $dOB,
+    'image' => $image,
+    'level' => $level,
+    'position' => $position,
+  );
+  array_push($data,$to_encode);
 }
 echo json_encode($data);
 ?>
--- wp4.7/wp-content/themes/twentyseventeen/functions.php
+++ wp4.7/wp-content/themes/twentyseventeen/functions.php
@@ -668,17 +668,24 @@ function prefix_get_endpoint_phrase($request) {
  */
 
  function prefix_get_endpoint_phrase_rating($request) {
-
 		$html = file_get_contents('http://cchc.danang.gov.vn/index.php?option=com_mucdohailong&controller=danhgiacongchuc&task=loadCanbo&format=raw&coquan=150945');
 		$arr = select_elements('#tasks .li_canbo img', $html);
 		$data = array();
+
 		foreach ($arr as $key => $value) {
-		    $to_encode = array('id' => $key,
-					'name' => select_elements('#tasks .li_canbo .lbl:nth-child(1)', $html)[$key]["text"],
-		      'dOB' => select_elements('#tasks .li_canbo .lbl:nth-child(2)', $html)[$key]["text"],
-		      'image' => $value["attributes"]["src"],
-		      'level' => select_elements('#tasks .li_canbo .lbl:nth-child(3)', $html)[$key]["text"],
-		      'position' => select_elements('#tasks .li_canbo .lbl:nth-child(4)', $html)[$key]["text"],
+		    $name = select_elements('#tasks .li_canbo .lbl:nth-child(1)', $html)[$key]["text"];
+		    $dOB = str_replace("Ngày sinh ","",select_elements('#tasks .li_canbo .lbl:nth-child(2)', $html)[$key]["text"]);
+		    $image = $value["attributes"]["src"];
+		    $level = str_replace("Trình độ học vấn: ","",select_elements('#tasks .li_canbo .lbl:nth-child(3)', $html)[$key]["text"]);
+		    $position = str_replace("Chức vụ: ","",select_elements('#tasks .li_canbo .lbl:nth-child(4)', $html)[$key]["text"]);
+
+		    $to_encode = array(
+					'id' => $key,
+					'name' => $name,
+		      'dOB' => $dOB,
+		      'image' => $image,
+		      'level' => $level,
+		      'position' => $position,
 		    );
 		    array_push($data,$to_encode);
 		}
